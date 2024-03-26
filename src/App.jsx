cca709eeb40f018906e5088c1e8ef7d420980112
Minesh Parekh <saixyyu@gmail.com>
2024-03-26 12:43:23 +0000

    removed services from nav and site

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Services from "./components/Services/Services";
+
 
 function App() {
   return (
@@ -14,7 +14,6 @@ function App() {
         <Home />
         <About />
         <Skills />
-        <Services />
       </main>
     </>
   );

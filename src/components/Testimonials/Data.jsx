0dcee0146b6281cbf8007ffbb4adbe374d3be6e5
Minesh Parekh <saixyyu@gmail.com>
2024-03-26 20:23:36 +0000

    added styling and queries for testimonials

--- src/components/Testimonials/Data.jsx
+++ src/components/Testimonials/Data.jsx
@@ -1,5 +1,6 @@
 import Image1 from "../../assets/testimonial-1.jpg"
 import Image2 from "../../assets/testimonial-2.jpg"
+import Image3 from "../../assets/testimonial-3.jpeg"
 
 export const Data = [
     {
@@ -7,13 +8,20 @@ export const Data = [
     image: Image1,
     title: "Chersilnt Ntervisi",
     description:
-"I had the pleasure of collaborating with Minesh on a group project during our Frontend Web Development Bootcamp. His dedication and technical acumen stood out throughout our final project. Minesh’s attention to detail, problem-solving skills, and ability to work seamlessly within a team were commendable. His contributions significantly elevated our project, and I wholeheartedly recommend Minesh for any front-end development role.",
+    `"Minesh’s attention to detail, problem-solving skills, and ability to work seamlessly within a team were commendable."`
     },
     {
     id: 2,
     image: Image2,
     title: "Toussaint Phillips",
     description:
-    "I highly recommend Minesh as a talented Web Developer. His proficiency in CSS is exceptional, enabling him to create visually stunning and user-friendly interfaces. His skills in design bring a unique aesthetic to his work wich is clean and eye-catching. Additionally, his expertise in React, HTML, and JavaScript ensures he can tackle any development challenge with ease. With his combination of technical prowess and design sensibility, Minesh would be a valuable asset to any team.",
+    `"With his combination of technical prowess and design sensibility, Minesh would be a valuable asset to any team."`,
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: "Niroshi Lawin",
+        description:
+        `"Minesh's ability to work harmoniously within a team, coupled with his outstanding communication skills, renders him an invaluable addition to any group."`
     },
 ];
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import "./Testimonials.css"
-import { Data } from './Data'
+import React from 'react';
+import "./Testimonials.css";
+import { Data } from './Data';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Pagination } from "swiper/modules"
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
     return (
     <section className="testimonial container section">
         <h2 className="section__title">Testimonials</h2>
-        <span className="section__subtitle">Past Experiences</span>
+        <span className="section__subtitle">Extended Recommendations on LinkedIn</span>
 
-        <div className="testimonial__container">
+        <Swiper className="testimonial__container"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+            clickable: true
+        }}
+        breakpoints={{
+            576: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 48,
+            },
+        }}
+        modules={[Pagination]}
+        >
             {Data.map(({id, image, title, description}) => {
                 return (
-                    <div className="testimonial__card" key={id}>
+                    <SwiperSlide className="testimonial__card" key={id}>
                         <img src={image} alt="" className='testimonial__img'/>
 
                         <h3 className="testimonial__name">{title}</h3>
                         <p className="testimonial__description">{description}</p>
-                    </div>
+                    </SwiperSlide>
                 )
             })}
-        </div>
+        </Swiper>
     </section>
     )
 }

b3c88c4c2f89a48308615433a838b5697f8eed14
Minesh Parekh <saixyyu@gmail.com>
2024-03-27 15:30:08 +0000

    fixed css import

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import "./contact.css"
+import "./Contact.css"
 
 const Contact = () => {
     const form = useRef();
